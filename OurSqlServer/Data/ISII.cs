f87ebaa8a25bacfe60ccc03c709bfd1bcc015987
KristianH123 <parkaranpreet@debian.org>
2018-10-29 11:52:27 +0100

    first commit

---
+++ OurSqlServer/Data/ISII.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurSqlServer.Data
+{
+    class ISII
+    {
+        //private List<List<Persona>> Listaaa = new List<List<!>>();
+
+        private List<Studenti> Alunni = new List<Studenti>();
+        private List<Insegnanti> Docenti = new List<Insegnanti>();
+
+
+        public ISII()
+        {
+            
+        }
+
+        public Alunno (Studenti A)
+        {
+
+        }
+    }
+
+}
---
+++ OurSqlServer/Data/persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurSqlServer.Data
+{
+    abstract class Persona
+    {
+        private string Nome;
+        private string Cognome;
+        private string NTelefono;
+
+        protected Persona(string n, string c, string nT)
+        {
+            Nome = n;
+            Cognome = c;
+            NTelefono = nT;
+        }
+        protected Persona()
+        {
+            Nome = "";
+            Cognome = "";
+            NTelefono = "0000000000";
+        }
+    }
+}
