cb6835bb98e212212cdb7f702c885e373ad1a4f2
FMocanu <florin.mocanu.studenti@isii.it>
2018-11-01 17:56:45 +0100

    Realizzazione interfaccia grafica SSMS, realizzazione estrapolazione dati studenti ed insegnanti da file

--- OurSqlServer/Form1.cs
+++ OurSqlServer/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using OurSqlServer.Data;
+using System.IO;
 
 namespace OurSqlServer
 {
@@ -20,9 +21,53 @@ namespace OurSqlServer
 
         private void frmSSMS_Load(object sender, EventArgs e)
         {
-            Insegnanti[,] myinsegnanti = new Insegnanti[3,3];
+            List<Insegnanti> myinsISII = new List<Insegnanti>();
+            List<Insegnanti> myinsTRAMELLO = new List<Insegnanti>();
+            List<Studenti> mystudISII = new List<Studenti>();
+            List<Studenti> mystudTRAMELLO = new List<Studenti>();
 
-            
+            string path_insISII =@"C:\Users\Mokanu\source\Repos\OurSqlServer\OurSqlServer\TextData\InsegnantiISII.txt";
+            string path_insTRAMELLO = @"C:\Users\Mokanu\source\Repos\OurSqlServer\OurSqlServer\TextData\InsegnantiTRAMELLO.txt";
+            string path_studISII = @"C:\Users\Mokanu\source\Repos\OurSqlServer\OurSqlServer\TextData\StudentiISII.txt";
+            string path_studTRAMELLO = @"C:\Users\Mokanu\source\Repos\OurSqlServer\OurSqlServer\TextData\StudentiTRAMELLO.txt";
+
+            var line_insISII = File.ReadLines(path_insISII);
+            var line_insTRAMELLO = File.ReadLines(path_insTRAMELLO);
+            var line_studISII = File.ReadLines(path_studISII);
+            var line_studTRAMELLO = File.ReadLines(path_studTRAMELLO);
+
+            foreach (var line in line_insISII)
+            {
+                string[] token = line.Split('@');
+                Insegnanti insegnantiISII = new Insegnanti(token[0], token[1], token[2]);
+                myinsISII.Add(insegnantiISII);
+                
+            }
+
+            foreach (var line in line_insTRAMELLO)
+            {
+                string[] token = line.Split('@');
+                Insegnanti insegnantiTRAMELLO = new Insegnanti(token[0], token[1], token[2]);
+                myinsTRAMELLO.Add(insegnantiTRAMELLO);
+            }
+
+            foreach (var line in line_studISII)
+            {
+                string[] token = line.Split('@');
+                Studenti studentiISII = new Studenti(token[0], token[1], token[2]);
+                mystudISII.Add(studentiISII);
+            }
+
+            foreach (var line in line_studTRAMELLO)
+            {
+                string[] token = line.Split('@');
+                Studenti studentiTRAMELLO = new Studenti(token[0], token[1], token[2]);
+                mystudTRAMELLO.Add(studentiTRAMELLO);
+            }
+
+            txtPassword.Text = mystudISII.ElementAt(2).Nome.ToString();
+
+           
         }
     }
 }
